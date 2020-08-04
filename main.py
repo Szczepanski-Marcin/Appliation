ae87dc3ba1bb4ef45a2b5c2e824927b534ebb482
Szczepanski-Marcin <szmar@vp.pl>
2020-08-04 22:43:30 +0200

    Added login window and back to main window

--- main.py
+++ main.py
@@ -24,6 +24,12 @@ class SignUpScreen(Screen):
         
         with open("users.json", "w") as file:
             json.dump(users, file)
+        self.manager.current = "sign_up_screen_success"
+
+class SignUpScreenSuccess(Screen):
+    def go_to_login(self):
+        self.manager.transition.direction = "right" #przech. str. w kier. prawym
+        self.manager.current = "login_screen"
 
 class MainApp(App):
     def build(self):
