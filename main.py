6a579925a8ce67a6e262c8fe08c1f68393f6e585
Szczepanski-Marcin <szmar@vp.pl>
2020-08-12 23:43:34 +0200

    Added logout button

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.animation import Animation
+from hoverable import HoverBehavior
+from kivy.uix.image import Image
+from kivy.uix.behaviors import ButtonBehavior
 import json, glob
 from datetime import datetime
 from pathlib import Path
 import random
+
 
 Builder.load_file('design.kv')
 
@@ -71,6 +75,9 @@ class LoginScreenSuccess(Screen):
             self.ids.quote.text = random.choice(quotes)
         else:
             self.ids.quote.text = "Try another feeling."
+
+class ImageButton(ButtonBehavior, HoverBehavior, Image):
+    pass
 
 
 class MainApp(App):
